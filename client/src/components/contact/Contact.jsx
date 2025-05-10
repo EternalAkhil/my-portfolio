cde65e027f75663b715999c9034275d842898f2d
EternalAkhil <akr142003@gmail.com>
2025-05-10 13:54:52 +0530

    update

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -7,9 +7,9 @@ const Contact = () => {
                 <p>Feel free to reach out</p>
             </div>
             <div className={styles.contacts}>
-                <div><img src="public/contact/emailIcon.png" alt="email" /><a href="mailto:akr142003@gmial.com">akr142003@gmial.com</a></div>
-                <div><img src="public/contact/githubIcon.png" alt="github" /><a href="https://github.com/EternalAkhil">github.com/EternalAkhil</a></div>
-                <div><img src="public/contact/linkedinIcon.png" alt="linkedin" /><a href="https://linkedin.com/in/akr-reddy">linkedin.com/akr-reddy</a></div>
+                <div><img src="contact/emailIcon.png" alt="email" /><a href="mailto:akr142003@gmial.com">akr142003@gmial.com</a></div>
+                <div><img src="contact/githubIcon.png" alt="github" /><a href="https://github.com/EternalAkhil">github.com/EternalAkhil</a></div>
+                <div><img src="contact/linkedinIcon.png" alt="linkedin" /><a href="https://linkedin.com/in/akr-reddy">linkedin.com/akr-reddy</a></div>
             </div>
 
         </footer>
